f48b72860c6ef55cf5460e7477eaf431a4a14373
ChaitanyaKampara <136173270+ChaitanyaKampara@users.noreply.github.com>
2024-03-01 10:39:03 +0530

    Add files via upload

---
+++ majorityElement.py
@@ -0,0 +1,28 @@
+def majorityElement(arr):
+    cnt = 0
+    ele = None
+    n = len(arr)
+
+    for i in range(n):
+        if cnt == 0:
+            cnt = 1       
+        elif ele == arr[i]:
+             cnt += 1
+        else:
+            cnt -= 1
+# until here we are fixing element ele
+
+    cnt1 = 0
+
+    for i in range(n):
+        if arr[i] == ele:
+            cnt1 += 1
+
+    if cnt1 > (n/2):
+        return ele
+    return -1
+# until here we are checking that ele element count by linear search
+
+arr = [1,1,1,1,1,2,2]
+ans = majorityElement(arr)
+print("The majority element is:" , ans)   
