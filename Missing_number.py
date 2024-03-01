55da4bbb3c11d6e74b78bd0f6d9fa51bb874008e
ChaitanyaKampara <136173270+ChaitanyaKampara@users.noreply.github.com>
2024-03-01 10:36:51 +0530

    Add files via upload

---
+++ Missing_number.py
@@ -0,0 +1,13 @@
+def missing_number(str):
+    n = str[::-1]
+    a = 0
+    a = sum(str)
+    k = (n*(n+1))//2
+    print(k-a)
+    print(n)
+
+   # lst = str.split()
+   # n = lst.length()
+   # k = ((n+1)(n+2))/2
+str = [1, 2, 3, 5, 6, 7]
+missing_number(str)
