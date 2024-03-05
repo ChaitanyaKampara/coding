fef0689f66579ef26d6b8372cf127b1614d2fc46
ChaitanyaKampara <136173270+ChaitanyaKampara@users.noreply.github.com>
2024-03-05 23:45:13 +0530

    Add files via upload

---
+++ rotate_matrix_90.py
@@ -0,0 +1,29 @@
+
+from typing import List
+def rotate(matrix: List[List[int]]) -> None:
+    n =len(arr)
+
+    for i in range(n):
+        for j in range(i):
+            arr[i][j] , arr[j][i] = arr[j][i] , arr[i][j]
+
+    for i in range(n):
+        arr[i].reverse()
+
+
+
+if __name__ == "__main__":
+    arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    rotate(arr)
+    print("Rotated Image")
+    for i in range(len(arr)):
+        for j in range(len(arr[0])):
+            print(arr[i][j], end=" ")
+        print()
+
+
+
+
+
+
+
